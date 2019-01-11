44e32e743aff76c0957c6020fbc83d765c4eb978
M1467 <M1467@student.jamk.fi>
2019-01-11 16:29:04 +0200

    Lab01 T4

---
+++ Lab01/T4/T4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Labra01 T4
+ * Tee ohjelma, jossa kysytään käyttäjältä tämän ikä. Jos ikä on alle 18 vuotta,
+ *  tulosta "alaikäinen", jos se on 18-65 vuotta, tulosta "aikuinen", muussa tapauksessa tulosta "seniori".
+  */
+namespace T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+        }
+    }
+}
