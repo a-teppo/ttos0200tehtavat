f794a7bc34c4da9caec560a505f685a378996f05
M1467 <M1467@student.jamk.fi>
2019-01-11 14:22:43 +0200

    Init weektask 1

---
+++ Lab00/T1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " ";
+            Console.WriteLine("Please, give your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello World and Hello {0}!", name);
+        }
+    }
+}
