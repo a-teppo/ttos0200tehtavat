53d311ebf6f4b8a2661fc81dba99426056f73c8a
Antti Teppo <m1467@student.jamk.fi>
2019-01-17 20:53:21 +0200

    Labra02 folder

---
+++ Lab02/T1/T1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Labra02 T1
+ * Tee kahden sisäkkäisen for-silmukan avulla ohjelma, joka tulostaa seuraavanlaisen kuvion:
+ * Käyttäjä antaa syötteenä tähtirivien lukumäärän
+ */
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+	        {	        
+		        
+	        }
+	        catch (Exception ex)
+	        {
+
+		        Console.WriteLine(ex.Message);
+	        }
+        }
+    }
+}
---
+++ Lab02/T2/T2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Labra02 T2
+ * Tee ohjelma, joka kysyy käyttäjältä 5 kokonaislukua. Luvut tulee sijoittaa taulukkoon. Ohjelman tulee tulostaa annetut luvut käänteisessä järjestyksessä.
+ */
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
