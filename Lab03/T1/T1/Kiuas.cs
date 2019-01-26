b2328f21c3e5fb8903fb347ce744a1b7a5d05bda
M1467 <M1467@student.jamk.fi>
2019-01-26 14:30:17 +0200

    Studies at school

---
+++ Lab03/T1/T1/Kiuas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Kiuas
+    {
+        // field variables
+        // properties
+        public string Merkki { get; set; }
+
+        // constructors
+        // methods
+        public bool Päällä(string päällä)
+        {
+            Console.WriteLine("Onko kiuas päällä? K = kyllä / E = ei");
+            päällä = Console.ReadLine();
+            if (päällä == "K" || päällä == "k")
+                return true;
+            else
+                return false;
+                
+        }
+        public int lämpötila;
+        public int kosteus;
+        // events
+        // indexer
+        // destructor
+    }
+}
--- Lab03/T1/T1/Program.cs
+++ Lab03/T1/T1/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace T1
+/*Lab03 T1
+ *Antti Teppo 26.1.2019
+ *Tehtävänäsi on ohjelmoida kiukaan toiminta. Kiuas pitää pystyä laittamaan päälle ja pois,
+ *sekä sen lämpötilaa että sen antamaa kosteutta pitää pystyä säätämään (arvoja ei ole rajattu).
+ */
+namespace Jamk.It.OO
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                
+                Kiuas kiuas = new Kiuas();
+                Kiuas kiuas2 = new Kiuas();
+                //asetetaan ominaisuuksia
+                kiuas.Merkki = "Harvia";
+                kiuas2.Merkki = "HomeMade";
+
+                Console.WriteLine("Onko kiuas päällä? K = kyllä / E = ei");
+                kiuas.Päällä = Console.ReadLine();
+                Console.WriteLine("Kiuas on {0}", kiuas.Päällä);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
