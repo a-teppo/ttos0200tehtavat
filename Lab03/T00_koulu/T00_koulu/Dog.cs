a29be51d0957ea58db5e5b2efc303aa10a9b8ce5
M1467 <M1467@student.jamk.fi>
2019-01-26 11:21:28 +0200

    update from school

--- Lab03/T00_koulu/T00_koulu/Dog.cs
+++ Lab03/T00_koulu/T00_koulu/Dog.cs
@@ -6,13 +6,73 @@ using System.Threading.Tasks;
 
 namespace Jamk.It.OO
 {
+    public enum Gender
+    {
+        Unknown,
+        Female,
+        Male
+    }
     public class Dog
     {
         // field variables
+        private string breed; //rotu taustamuuttuja
         // properties
         public string Name { get; set; }
+        public string Color { get; set; }
+        //vain luettavissa
+        public Gender DogGender { get; }
+        //Rotu ominaisuus on vain luettavissa, sitä ei pysty muuttamaan
+        //asetetaan konstruktorissa
+        public string Breed
+        {
+            get
+            {
+                return breed;
+            }
+        }
         // constructors
+        public Dog()
+        {
+            //oletuskonstruktori
+            //asetetaan niin että koira syntyy mustana
+            Color = "musta";
+            //rotu
+            breed = "unknown";
+        }
+        public Dog(Gender gender)
+        {
+            Color = "musta";
+            //asetetaan koiran sukupuoli olion luonnin aikana "syntymän" aikana.
+            DogGender = gender;
+            breed = "unknown";
+        }
+        public Dog(string name, string color, Gender gender)
+        {
+            Name = name;
+            Color = color;
+            DogGender = gender;
+            breed = "unknown";
+        }
+        public Dog(string name, string color, Gender gender, string breed) :
+            this(name, color, gender)
+        {
+            this.breed = breed;
+        }
         // methods
+        public string Growl()
+        {
+            return "murr murr";
+        }
+        public string Bark(int times)
+        {
+            // some interesting barking action happens here
+            string bark = "";
+            for (int i = 0; i < times; i++)
+            {
+                bark += i+1 + ". vuh ";
+            }
+            return bark;
+        }
         // events
         // indexer
         // destructor
--- Lab03/T00_koulu/T00_koulu/Program.cs
+++ Lab03/T00_koulu/T00_koulu/Program.cs
@@ -13,19 +13,47 @@ namespace Jamk.It.OO
 {
     class DogDemo
     {
+        static void ShowDogsData(List<Dog> koirat)
+        {
+            foreach (var item in koirat)
+            {
+                Console.WriteLine("Koira nimeltä {0} on väriltään {1} ja sukupuoleltaan {2} sekä rodultaan {3}", item.Name, item.Color, item.DogGender, item.Breed);
+            }
+        }
         static void Main(string[] args)
         {
             try
             {
                 // luodaan koira olioita
-                Dog koira = new Dog();
-                Dog koira2 = new Dog();
+                Dog koira = new Dog(Gender.Male);
+                Dog koira2 = new Dog(Gender.Male);
+                Dog tyttokoira = new Dog(Gender.Female);
+                Dog poikakoira = new Dog(Gender.Male);
                 // asetetaan ominaisuuksia
                 koira.Name = "Sessa";
+                koira.Color = "Musta";
                 koira2.Name = "Jaska";
+                //koira2.Color = "Ruskea pilkullinen";
+                tyttokoira.Name = "Sussie";
+                tyttokoira.Color = "Valkoinen";
+                poikakoira.Name = "Sepe";
                 // luodaan koiratarha (kokoelma koiria)
+                List<Dog> koirat = new List<Dog>();
+                //lisätään meidän koirat listaaan
+                koirat.Add(koira);
+                koirat.Add(koira2);
+                koirat.Add(tyttokoira);
+                koirat.Add(poikakoira);
+                koirat.Add(new Dog("Pikke", "vaalea", Gender.Male));
+                koirat.Add(new Dog("Camilla", "Harmaa", Gender.Female, "susikoira"));
                 // näytetään koirien ominaisuudet
-                Console.WriteLine("Minulla on kaksi koiraa nimeltään {0} ja {1}", koira.Name, koira2.Name);
+                ShowDogsData(koirat);
+                // Console.WriteLine("Minulla on kaksi koiraa nimeltään {0} ja {1}", koira.Name, koira2.Name);
+                // Console.WriteLine("Koiran {0} väri {1} ja sukupuoli {2}", koira.Name, koira.Color, koira.DogGender);
+                // laitetaan Jaska murisemaan ja haukkumaan
+                // Console.WriteLine(koira2.Growl());
+                // Console.WriteLine(koira2.Bark(10));
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
