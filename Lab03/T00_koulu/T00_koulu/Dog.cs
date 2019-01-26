05a2c6bad00c2489031903c19494d9100479dff6
M1467 <M1467@student.jamk.fi>
2019-01-26 09:47:26 +0200

    Lab03 New_things_at_school

---
+++ Lab03/T00_koulu/T00_koulu/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Dog
+    {
+        // field variables
+        // properties
+        public string Name { get; set; }
+        // constructors
+        // methods
+        // events
+        // indexer
+        // destructor
+    }
+}
---
+++ Lab03/T00_koulu/T00_koulu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Antti Teppo
+ *26.1.2019
+ *Demoa koulussa
+ * 
+ * 
+ */
+namespace Jamk.It.OO
+{
+    class DogDemo
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // luodaan koira olioita
+                Dog koira = new Dog();
+                Dog koira2 = new Dog();
+                // asetetaan ominaisuuksia
+                koira.Name = "Sessa";
+                koira2.Name = "Jaska";
+                // luodaan koiratarha (kokoelma koiria)
+                // näytetään koirien ominaisuudet
+                Console.WriteLine("Minulla on kaksi koiraa nimeltään {0} ja {1}", koira.Name, koira2.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
