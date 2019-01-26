4c43f8e07ff9c6a7d34cd93199d75697e46a83b4
M1467 <M1467@student.jamk.fi>
2019-01-26 13:15:12 +0200

    update from school2

---
+++ Lab03/T00_koulu/T01_koulu/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Car
+    {
+        // fields
+        private int speed;
+        private const int maxSpeed = 200; //vakio
+        //properties
+        public int Speed { get { return speed; } }
+        //constructors ei tarvita
+        //methods
+        public bool Accelerate(int more)
+        {
+            if (speed + more > maxSpeed)
+            {
+                speed = maxSpeed;
+                return false;
+            }
+            else
+            {
+                speed = speed + more;
+                return true;
+            }
+        }
+
+        public void Brake(int less)
+        {
+            if (speed - less < 0)
+            {
+                speed = 0;
+            }
+            else
+            {
+                speed = speed - less;
+            }
+        }
+
+    }
+}
---
+++ Lab03/T00_koulu/T01_koulu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*TTOS0200
+ *Antti Teppo 26.1.2019
+ */
+
+namespace Jamk.It.OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // luodaan auto olio
+                bool kiihtyikö = false;
+                Car car = new Car();
+                // kuinka kovaa päästään
+                for (int i = 0; i < 300; i++)
+                {
+                    Console.WriteLine("Nopeus on: {0} ", car.Speed);
+                    kiihtyikö = car.Accelerate(i);
+                    if (kiihtyikö)
+                        Console.WriteLine("Vauhti sen kuin kiihtyy");
+                    else
+                        Console.WriteLine("Älä hyydy kone!");
+                    //perskules polliisi puskassa, jarrutetaan!
+                    if (car.Speed > 100)
+                        car.Brake(10);
+                }
+                // lopetus
+                Console.WriteLine("Bensa loppu ja mittari näytti: {0} ", car.Speed);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
