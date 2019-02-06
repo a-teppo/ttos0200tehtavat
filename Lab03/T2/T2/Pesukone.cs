1b5b74d303b60478d427712c75f59a3b8923ff92
Antti Teppo <m1467@student.jamk.fi>
2019-02-06 13:11:39 +0200

    Lab03 T2 return

---
+++ Lab03/T2/T2/Pesukone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Pesukone
+    {
+        // field variables
+        // properties
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        public int Koko { get; set; }
+        public string Pesuohjelma { get; set; }
+        public bool Kytkin { get; set; }
+        // constructors
+        public Pesukone ()
+        {
+            Merkki = "Bosch";
+            Koko = 8;
+        }
+        public Pesukone(string merkki, string malli, int koko, string pesuohjelma, bool kytkin)
+        {
+            Merkki = merkki;
+            Malli = malli;
+            Koko = koko;
+            Pesuohjelma = pesuohjelma;
+            Kytkin = kytkin;
+        }
+        // methods
+        public string Pesuohjelmanvalinta()
+        {
+            int Valinta=0;
+            Console.WriteLine("Valitse haluttu pesuohjelma: 1 = valkopyykki, 2 = kirjopesu tai 3 = hienopesu");
+            Valinta = int.Parse(Console.ReadLine());
+            switch (Valinta)
+            {
+                case 1:
+                    Console.WriteLine("Valittu pesuohjelmaksi: valkopyykki");
+                    Pesuohjelma = "valkopyykki";
+                    break;
+                case 2:
+                    Console.WriteLine("Valittu pesuohjelmaksi: kirjopesu");
+                    Pesuohjelma = "kirjopesu";
+                    break;
+                case 3:
+                    Console.WriteLine("Valittu pesuohjelmaksi: hienopesu");
+                    Pesuohjelma = "hienopesu";
+                    break;
+            }
+            return Pesuohjelma;
+        }
+        // events
+        // indexer
+        // destructor
+    }
+}
---
+++ Lab03/T2/T2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Lab03 T2
+ *6.2.2019 Antti Teppo
+ *Tehtävänäsi on ohjelmoida pesukoneen toiminta. 
+ *Toteuta Pesukone-luokan ohjelmointi sekä pääohjelma, jolla luot olion Pesukone-luokasta.
+ *Säädä pesukone-oliota erilaisilla arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi, jotta pesukone-olion käyttäminen jää näkyville.
+ *Toteuta Pesukone-luokkaan muutamia erilaisia konstruktoreita ja alusta niitä käyttämällä oliota pääohjelmasta käsin.
+ */
+
+namespace Jamk.It.OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Pesukone lgA = new Pesukone("LG","tnt",7,"hienopesu",true);
+                Pesukone bosch = new Pesukone();
+                Console.WriteLine("Konstruktoreilla alustetut tiedot:");
+                Console.WriteLine("1. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", lgA.Merkki, lgA.Malli, lgA.Koko, lgA.Pesuohjelma, lgA.Kytkin);
+                Console.WriteLine("2. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", bosch.Merkki, bosch.Malli, bosch.Koko, bosch.Pesuohjelma, bosch.Kytkin);
+                Console.WriteLine("************************");
+                Console.WriteLine("Valitaan pesukoneelle 1. pesuohjelma!");
+                lgA.Pesuohjelmanvalinta();
+                Console.WriteLine("1. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", lgA.Merkki, lgA.Malli, lgA.Koko, lgA.Pesuohjelma, lgA.Kytkin);
+                Console.WriteLine("************************ jatka ***********");
+                Console.ReadLine();
+                Console.WriteLine("Valitaan pesukoneelle 2. pesuohjelma!");
+                bosch.Pesuohjelmanvalinta();
+                Console.WriteLine("2. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", bosch.Merkki, bosch.Malli, bosch.Koko, bosch.Pesuohjelma, bosch.Kytkin);
+                Console.WriteLine("************************ jatka ***********");
+                Console.ReadLine();
+                Console.WriteLine("Alustetaan vielä 2. pesukoneen tietoja ja tulostetaan molempien olioiden tiedot näytölle");
+                bosch.Malli = "SilverFox";
+                bosch.Kytkin = true;
+                Console.WriteLine("************************ jatka ***********");
+                Console.ReadLine();
+                Console.WriteLine("1. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", lgA.Merkki, lgA.Malli, lgA.Koko, lgA.Pesuohjelma, lgA.Kytkin);
+                Console.WriteLine("2. Pesukone olion tietoja merkki: {0}, malli: {1}, koko: {2}, pesuohjelma: {3}, kytkimen asento: {4}", bosch.Merkki, bosch.Malli, bosch.Koko, bosch.Pesuohjelma, bosch.Kytkin);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
