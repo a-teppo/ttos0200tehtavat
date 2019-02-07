b1366a224061bd9e97f78e3b41a416d98b041d9b
Antti Teppo <m1467@student.jamk.fi>
2019-02-07 13:48:10 +0200

    Lab03 T4 return

---
+++ Lab03/T4/T4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Lab03T4
+ *7.2.2019 Antti Teppo
+ * Luo Vehicle-luokka seuraavien tietojen mukaisesti: Name:string, Speed:int,Tyres:int.
+ * Toiminnot:
+ * PrintData(), tulostaa Vehiclen ominaisuudet näytölle
+ * ToString():string, palauttaa kaikki Vehiclen ominaisuudet yhtenä merkkijonona
+ * Toteuta Vehicle-luokan ohjelmointi sekä pääohjelma, jolla luot olion Vehicle-luokasta. Muuta olion arvoja ja tulosta olion arvoja konsolille.
+ */
+namespace Jamk.It.OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Vehicle car = new Vehicle("Kupla", 125, 16);
+                car.PrintData();
+                car.Name = "Rellu";
+                car.Speed = 999;
+                car.Tyres = 19;
+                car.PrintData();
+                car.ToString(car.Name,car.Speed,car.Tyres);
+                for (int i = 0; i < car.laskuri; i++)
+                {
+                    Console.Write(car.lause[i]);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab03/T4/T4/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Vehicle
+    {
+        public string lause;
+        public int laskuri;
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+        //constructor
+        public Vehicle(string name, int speed, int tyres)
+        {
+            Name = name;
+            Speed = speed;
+            Tyres = tyres;
+        }
+        //methods
+        public void PrintData()
+        {
+            Console.WriteLine("Auton Nimi: {0}, Nopeus: {1} ja Renkaat {2}", Name, Speed, Tyres);
+        }
+        public string ToString(string name, int speed, int tyres)
+        {
+            
+            lause = Name +Speed + Tyres;
+            laskuri = lause.Length;
+            return lause;
+        }
+
+    }
+}
