9ddead4bc1c81ee641839337247a081ce5f30404
Antti Teppo <m1467@student.jamk.fi>
2019-02-07 12:10:50 +0200

    Lab03 T3 return

--- Lab03/T3/T3/Program.cs
+++ Lab03/T3/T3/Program.cs
@@ -16,7 +16,47 @@ namespace Jamk.It.OO
         {
             try
             {
-
+                Televisio Tv1 = new Televisio("Samsung", "kuvaputki", 32);
+                Console.WriteLine("Onnittelut uudet television hankinnasta!");
+                Console.WriteLine("Merkki: {0}, Malli: {1}, Koko: {2}, ", Tv1.Merkki, Tv1.Malli, Tv1.Koko);
+                Console.WriteLine("Käynnistetäänkö televsio? Kyllä = 1 ja kaikki muut valinnat Ei.");
+                string valinta;
+                valinta = Console.ReadLine();
+                Tv1.Käynnistä(valinta);
+                while (Tv1.Käynnistys)
+                {
+                    while (Tv1.Käynnistys)
+                    {
+                        Console.WriteLine("Valitse haluamasi kanava?");
+                        Console.WriteLine("1 = Yle 1");
+                        Console.WriteLine("2 = Yle 2");
+                        Console.WriteLine("3 = MTV 3");
+                        Console.WriteLine("4 = Nelonen");
+                        Console.WriteLine("0 = Sammutus");
+                        int kanava;
+                        kanava = int.Parse(Console.ReadLine());
+                        Tv1.ValitseKanava(kanava);
+                        Console.WriteLine("Olion tila:");
+                        Console.WriteLine("Merkki: {0}, Malli: {1}, Koko: {2}, Kanava: {3}, ÄÄni {4}", Tv1.Merkki, Tv1.Malli, Tv1.Koko, Tv1.Kanava, Tv1.Ääni);
+                        while (Tv1.Säädöt && Tv1.Käynnistys)
+                        {
+                                    Console.WriteLine("Säädä äänenvoimakkuus haluammallesi tasolle? (0-100)");
+                                    int ääni;
+                                    ääni = int.Parse(Console.ReadLine());
+                                    Tv1.SäädäÄäni(ääni);
+                                    Console.WriteLine("Olion tila:");
+                                    Console.WriteLine("Merkki: {0}, Malli: {1}, Koko: {2}, Kanava: {3}, Ääni: {4}", Tv1.Merkki, Tv1.Malli, Tv1.Koko, Tv1.Kanava, Tv1.Ääni);
+                                    Console.WriteLine("Onko äänenvoimakkuus nyt sopiva? Kyllä = 1 ja kaikki muut valinnat niin voit säätää uudestaan.");
+                                    valinta = Console.ReadLine();
+                                    Tv1.Säädä(valinta);
+                        }
+                    }
+                Console.WriteLine("Haluatko todella sammuttaa television? Pidä televisio käynnissä = 1 ja sammutus on mikä tahansa muu valinta");
+                valinta = Console.ReadLine();
+                Tv1.Käynnistä(valinta);
+                }
+                Console.WriteLine("Palaathan pian katsomaan televisiota. Hyvää päivän jatkoa!");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
--- Lab03/T3/T3/televisio.cs
+++ Lab03/T3/T3/televisio.cs
@@ -6,12 +6,79 @@ using System.Threading.Tasks;
 
 namespace Jamk.It.OO
 {
-    public class televisio
+    public class Televisio
     {
         // field variables
+        private bool isOn = false;
+        private bool isOK = true;
+        private int ääni;
         // properties
+        public string Merkki {get; set;}
+        public string Malli {get; set;}
+        public int Koko {get; set;}
+        public string Kanava {get; set;}
+        public int Ääni {get {return ääni;}
+                        set {
+                            if (value >= 0 && value <= 100) {ääni = value;}
+                            else ääni = -10;
+                            }
+                        }
+        public bool Käynnistys {get { return isOn;}}
+        public bool Säädöt {get { return isOK;}}
         // constructors
+        public Televisio(string merkki, string malli, int koko)
+           // this(merkki, malli, koko);
+        {
+            Merkki = merkki;
+            Malli = malli;
+            Koko = koko;
+        }
         // methods
+        public bool Käynnistä(string valinta)
+        {
+            if (valinta == "1")
+                return isOn = true;
+            else
+                return isOn = false;
+        }
+        public string ValitseKanava(int kanava)
+        {
+            switch (kanava)
+            {
+            case 1:
+                    Kanava = "Yle 1";
+                    break;
+            case 2:
+                    Kanava = "Yle 2";
+                    break;
+            case 3:
+                    Kanava = "MTV 3";
+                    break;
+            case 4:
+                    Kanava = "Nelonen";
+                    break;
+            case 0:
+                    Kanava = "tyhjä";
+                    Käynnistä("0");
+                    break;
+             default:
+                    Console.WriteLine("Tunnistamaton valinta!");
+                    break;
+            }
+            return Kanava;
+        }
+        public int SäädäÄäni(int ääni)
+            {
+                Ääni = ääni;
+                return Ääni;
+            }
+        public bool Säädä(string valinta)
+        {
+            if (valinta == "1")
+                return isOK = false;
+            else
+                return isOK = true;
+        }
         // events
         // indexer
         // destructor
