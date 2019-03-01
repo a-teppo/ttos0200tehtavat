646a664fd51ed7675ea2119a01033da7a137111f
Antti Teppo <m1467@student.jamk.fi>
2019-03-01 14:10:39 +0200

    Lab03T6 return

---
+++ Lab03/T6/T6/Liiga.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Liiga
+    {
+        public string TeamName { get; set; }
+        public int PlayedGames { get; set; }
+        public int Wins { get; set; }
+        public int GoalDifference { get; set; }
+
+        public Liiga(string teamname, int playgames, int wins, int goaldifference)
+        {
+            TeamName = teamname;
+            PlayedGames = playgames;
+            Wins = wins;
+            GoalDifference = goaldifference;
+        }
+
+        public void AddWin()
+        {
+            Wins += 1;
+        }
+
+        public void UpdateGoalDifference(int goals)
+        {
+            GoalDifference += goals;
+        }
+
+       
+    }
+}
---
+++ Lab03/T6/T6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Lab03 T6
+ * Antti Teppo
+ * 01.03.2019
+ * Valitse jokin reaalimaailman asia ja suunnittele siitä luokkarakenne UML-editorilla. Tee uusi projekti.
+ * Ohjelmoi suunnittelema luokkasi sekä käytä tekemääsi luokkaa edellisten tehtävien mukaisesti.
+ */
+namespace T6
+{
+    class Program
+    {
+
+        static void PrintData(List<Liiga> teamtable)
+        {
+            foreach (var item in teamtable)
+            {
+                Console.WriteLine("Joukkue: {0} Pelit: {1} Voitot: {2} ja Maaliero: {3}", item.TeamName, item.PlayedGames, item.Wins, item.GoalDifference);
+            }
+        }
+        static void TestLiiga()
+        {
+            Liiga Tappara = new Liiga("Tappara", 12, 8, 23);
+            Liiga Ilves = new Liiga("Ilves", 12, 4, -6);
+            Liiga Jyp = new Liiga("Jyp", 12, 6, 4);
+            Liiga Lukko = new Liiga("Lukko", 12, 1, -67);
+            List<Liiga> teamtable = new List<Liiga>();
+            teamtable.Add(Tappara);
+            teamtable.Add(Ilves);
+            teamtable.Add(Jyp);
+            teamtable.Add(Lukko);
+            PrintData(teamtable);
+            Console.WriteLine("************************************************************");
+            Lukko.AddWin();
+            Console.WriteLine("Lisätty Lukolle yksi voitto: {0}", Lukko.Wins);
+            Lukko.UpdateGoalDifference(5);
+            Console.WriteLine("Laskettu Lukon maaliero uusiksi: {0}", Lukko.GoalDifference);
+            Console.WriteLine("************************************************************");
+            PrintData(teamtable);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestLiiga();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
