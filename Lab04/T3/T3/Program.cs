9fa4196e3305231ae113993a9028c7bb62c60d2f
Antti Teppo <m1467@student.jamk.fi>
2019-03-02 20:21:50 +0200

    Lab04 T3 return

---
+++ Lab04/T3/T3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Lab04 T3
+ * Antti Teppo
+ * 02.03.2019
+ * Tehtävänäsi on ohjelmoida yksinkertaisen vahvistimen toiminta, jolla voidaan kontrolloida äänenvoimakkuutta välillä 0-100.
+ * Toteuta Vahvistin-luokka ja tee pääohjelma, jolla luot olion Vahvistin-luokasta. Säädä ja kokeile vahvistinta eri äänenvoimakkuuksilla.
+ * Käytä Vahvistin-luokassa get- ja set-aksessoreita. get-aksessori palauttaa äänenvoimakkuuden ja set-aksessori rajaa äänenvoimakkuuden arvoa.
+ */
+
+namespace T3
+{
+    class Program
+    {
+        static void TestVahvistin()
+        {
+            Vahvistin thomson = new Vahvistin("Thomson");
+            while (true)
+            {
+                Console.WriteLine("Give a new volume value (0-100)");
+                thomson.Volume = int.Parse(Console.ReadLine());
+                thomson.ChangeVolume(thomson.Volume);
+                Console.WriteLine("-> Amplifier volume is set to: {0} ", thomson.Volume);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestVahvistin();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab04/T3/T3/Vahvistin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Tehtävänäsi on ohjelmoida yksinkertaisen vahvistimen toiminta, jolla voidaan kontrolloida äänenvoimakkuutta välillä 0-100.
+ * Toteuta Vahvistin-luokka ja tee pääohjelma, jolla luot olion Vahvistin-luokasta.Säädä ja kokeile vahvistinta eri äänenvoimakkuuksilla.
+ * Käytä Vahvistin-luokassa get- ja set-aksessoreita.get-aksessori palauttaa äänenvoimakkuuden ja set-aksessori rajaa äänenvoimakkuuden arvoa.
+*/
+namespace T3
+{
+    public class Vahvistin
+    {
+        private int volume;
+        private string name;
+
+        public string Name { get; set; }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100) volume = value;
+                else if (value > 100) volume = 100;
+                else if (value < 0) volume = 0;
+            }
+        }
+
+        public Vahvistin (string name)
+        {
+            Name = name;
+        }
+
+        public int ChangeVolume(int volume)
+        {
+            return volume;
+        }
+    }
+}
