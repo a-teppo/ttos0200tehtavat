d41dcc1fe48217594fe450b7b1c416e0d280fd97
Antti Teppo <m1467@student.jamk.fi>
2019-03-05 17:11:56 +0200

    Lab06 T5 return

---
+++ Lab06/T5/T5/BLTappara.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Tappara
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+
+        public Tappara(string name, int age, string address, double salary)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Address: {Address}, Salary: {Salary}";
+        }
+    }
+
+    class Players : Tappara
+    {
+        public string Position { get; set; }
+        public string ContractExpiry { get; set; }
+
+        public Players(string name, int age, string address, double salary, string position, string contractexpiry)
+            :base(name,age,address,salary) 
+        {
+            Position = position;
+            ContractExpiry = contractexpiry;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Position: {Position}, ContractExpiry {ContractExpiry} ";
+        }
+    }
+
+    class Stuff : Tappara
+    {
+        public string Role { get; set; }
+
+        public Stuff(string name, int age, string address, double salary, string role)
+            : base(name, age, address, salary)
+        {
+            Role = role;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Role: {Role}";
+        }
+    }
+}
---
+++ Lab06/T5/T5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Lab06 T5
+ * 5.3.2019
+ * Antti Teppo
+ * 
+ * Pohdi jokin reaalimaailman asia, jonka kautta voit toteuttaa pienimuotoisen C#-ohjelman/toteutuksen, joka käyttää perintää.
+ *
+ */
+namespace Jamk.It.OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Players player1 = new Players("Timo Susi", 48, "Mottitie 5", 25000, "forward", "expired");
+                Players player2 = new Players("Janne Sutinen", 34, "Kenkätie 1", 34000,"goalie", "5.6.2019");
+                Players player3 = new Players("Mikko Mallikas", 29, "Pottu 2", 12000, "defender", "5.6.2022");
+                Stuff stuff1 = new Stuff("Simo Salminen", 56, "Piparitie 4", 340000, "Coach");
+                Stuff stuff2 = new Stuff("Sari Salminen", 54, "Piparitie 4", 290000, "Manager");
+                List<Tappara> members = new List<Tappara>();
+                members.Add(player1);
+                members.Add(player2);
+                members.Add(player3);
+                members.Add(stuff1);
+                members.Add(stuff2);
+                foreach (var item in members)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
