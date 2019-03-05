52262621dfa459d4e507053703ea3fa137b7e654
Antti Teppo <m1467@student.jamk.fi>
2019-03-05 13:47:31 +0200

    Lab06T3 return

---
+++ Lab06/T3/T3/BLRadio.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamk.It.OO
+{
+    public class Radio
+    {
+        private bool on = false;
+        private string name;
+        private int volume = 0;
+        private double channel = 2000.0;
+
+        public string Name { get; }
+        public string Model { get; set; }
+        public bool On
+        {
+            get
+            {
+                return on;
+            }
+            set
+            {
+                On = value;
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= 10 && value >= 0) volume = value;
+                else
+                    volume = 0;
+            }
+        }
+        public double Channel
+        {
+            get
+            {
+                return channel;
+            }
+            set
+            {
+                if (value <= 26000.0 && value >= 2000.0) channel = value;
+                else
+                    channel = 2000.0;
+            }
+        }
+
+        public Radio(string name, string model)
+        {
+            Name = name;
+            Model = model;
+        }
+
+        public bool ToOn(bool changeOn)
+        {
+            on = changeOn;
+            return on;
+        }
+
+        public int ChangeVolume(int changeVolume)
+        {
+            Volume = changeVolume;
+            return Volume;
+        }
+
+        public double ChangeChannel(double changeChannel)
+        {
+            Channel = changeChannel;
+            return Channel;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Model: {Model}, On: {On}, Volume: {Volume}, Channel: {Channel}. ";
+        }
+    }
+}
---
+++ Lab06/T3/T3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Lab06 T3
+ * 5.3.2019
+ * Antti Teppo
+ * Tehtävässä tulee toteuttaa C#-ohjelma radion perustoimintojen testaamiseen.
+* Kannettavassa radiossa on vain kolme säädintä: päälle/pois-kytkin, äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0).
+* Laadi luokka kannettavan radion toteutukseksi. Käytä tekemääsi luokkaa pääohjelmasta käsin, eli testaile radion toimivuutta erilaisilla voimakkuuden ja taajuuden arvoilla.
+* Jätä asetus- ja tulostuslauseet näkyville pääohjelmaan, jotta radio-olion käyttö voidaan todentaa.
+*/
+
+namespace Jamk.It.OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio sony = new Radio("Sony", "old-T");
+            Console.WriteLine(sony.ToString());
+            sony.ToOn(true);
+            sony.Volume = 12;
+            Console.WriteLine(sony.ToString());
+            sony.Volume = -1;
+            Console.WriteLine(sony.ToString());
+            sony.Volume = 4;
+            Console.WriteLine(sony.ToString());
+            sony.ChangeChannel(25500.7);
+            sony.ChangeVolume(8);
+            Console.WriteLine(sony.ToString());
+            sony.ChangeChannel(29500.7);
+            sony.ChangeVolume(-3);
+            Console.WriteLine(sony.ToString());
+            Console.ReadLine();
+        }
+    }
+}
