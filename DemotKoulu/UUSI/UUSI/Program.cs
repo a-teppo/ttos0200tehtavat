d692ec71badcb0a18bfee155268b07a082cf3c5b
M1467 <M1467@student.jamk.fi>
2019-03-09 10:37:29 +0200

    koulu

--- DemotKoulu/UUSI/UUSI/Program.cs
+++ DemotKoulu/UUSI/UUSI/Program.cs
@@ -3,37 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*Lab06 T2
- * 9.2.2019 Antti Teppo
- * Toteutettavassa sovelluksessa tulisi pysyä käsittelemään erilaisia kulkuneuvoja.
- * Kaikilla kulkuneuvoilla on löydettävissä yhteisinä ominaisuuksina: nimi, malli, vuosimalli ja väri.
- * Erikoistapauksina pitää pystyä käsittelemään polkupyöriä ja veneitä.
- * Polkupyörän osalta pitää pystyä erottelemaan se, että onko kyseessä vaihdepyörä vai ei sekä mahdollisen vaihteiston mallinimi.
- * Veneiden osalta tietoina pitää ainakin olla veneen tyyppi (soutuvene, moottorivene, kajakki, ...) ja kuinka monta istuinpaikkaa veneestä löytyy.
- * Tutki tehtävän tavoitetta/kerrontaa ja toteuta tarvittavat UML-luokkakaaviot. Toteuta tämän jälkeen vaaditut luokat,
- * luo ja käytä olioita pääohjelmasta. Tulosta vaadittujen luokkien olioiden tietoja output-ikkunaan.
- * Tietoja ei tarvitse kysyä sovelluksen käyttäjältä, vaan voit alustaa ne suoraan pääohjelmassa.
+/*
  */
-namespace UUSI
+namespace Jamk.It.OO
 {
-    class Program
+    struct Ohjastajat
     {
-        static void TestVehicle()
-            {
-            
-            }
-        static void Main(string[] args)
-        {
-            Bike bike = new Bike("Jopo", "Street", 2016, "Blue");
-            bike.GearWheel = false;
-            bike.GearName = "";
-            Bike bike1 = new Bike("Tunturi", "StreetPower", 2010, "Black");
-            bike1.GearWheel = true;
-            bike1.GearName = "Shimano";
-            Console.WriteLine("Name: {0}, Model: {1}, ModelYear: {2}, Color: {3}, GearWheel: {4}, GearName: {5}", bike.Name, bike.Model, bike.ModelYear, bike.Color, bike.GearWheel, bike.GearName);
-            Console.WriteLine("Name: {0}, Model: {1}, ModelYear: {2}, Color: {3}, GearWheel:  {4}, GearName: {5}", bike1.Name, bike1.Model, bike1.ModelYear, bike1.Color, bike1.GearWheel, bike1.GearName);
-
-            Console.ReadLine();
-        }
+        public string Nimi;
+        public int Startit;
+        public int Voitot;
+        public int VoittoProsentti;
     }
-}
+
+        public class OhjastajatTiedot
+        {
+
+        static void LueOhjastajatVerStruct() 
+            {
+                try 
+	                {	        
+		
+                        // luetaan ohjastaja tiedot kerralla sisään ja muutetaan kukin rivi struktiksi
+                        string sep = ";";
+                        string filename = @"Z:\ohjelmoinnin_perusteet\ohjastajat2018.txt";
+                        string[] lines = System.IO.File.ReadAllLines(filename);
+                        // käydään array läpi ja muutetaan rivi Ohjastajaksi
+                        Ohjastajat kuski;
+                        int cnt = lines.Length;
+                        Console.WriteLine($"Ohjastajia tiedostossa {cnt}");
+                        //TODO
+                        for (int i = 1; i<cnt; i++)
+                        {
+                            // erotellaan riviltä sanat, erottimena on
+                            string[] words = lines[i].Split(sep.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                            if (words.Length > 2)
+                            {
+                                    kuski.Nimi = words[0];
+                                    kuski.Startit = int.Parse(words[1]);
+                                    kuski.Voitot = int.Parse(words[2]);
+                                    // lasketaan voittoprosentti
+                                    kuski.VoittoProsentti = (100F*kuski.Voitot/kuski.Startit);
+                                    // tulos näkyviin
+                                    Console.WriteLine($"Kuski {kuski.Nimi} ajoi {kuski.Startit} lähtöä ja voittoprosentti oli {kuski.VoittoProsentti}");
+                            }
+
+                        }
+                        // lopetus
+                        Console.WriteLine("That's all folks!");
+                        Console.ReadLine();
+                    
+	                 }
+	                    catch 
+	                    {
+                            throw;
+	                    }
+            }
+
+        }
+
+         static void Main(string[] args)
+        {
+            try 
+	        {	        
+		        LueOhjastajatVerStruct();
+	        }
+	        catch (Exception ex)
+	        {
+                  Console.WriteLine("Ohjelman suorituksessa tapahtui virhe " + ex.Message);
+	        }
+        }
+}    
+
